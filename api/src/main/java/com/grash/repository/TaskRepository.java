691cffe8e3dd0f0de4e3006fcaca82c803d1e2bc
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-03-27 22:38:39 +0000

    fix: tasks from PM not showing on generated WO

--- api/src/main/java/com/grash/repository/TaskRepository.java
+++ api/src/main/java/com/grash/repository/TaskRepository.java
@@ -4,9 +4,10 @@ import com.grash.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    Collection<Task> findByWorkOrder_Id(Long id);
+    List<Task> findByWorkOrder_Id(Long id);
 
-    Collection<Task> findByPreventiveMaintenance_Id(Long id);
+    List<Task> findByPreventiveMaintenance_Id(Long id);
 }
--- api/src/main/java/com/grash/service/ScheduleService.java
+++ api/src/main/java/com/grash/service/ScheduleService.java
@@ -12,11 +12,13 @@ import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
     private final PreventiveMaintenanceService preventiveMaintenanceService;
@@ -86,6 +88,7 @@ public class ScheduleService {
                     WorkOrder savedWorkOrder = workOrderService.create(workOrder);
                     tasks.forEach(task -> {
                         Task copiedTask = new Task(task.getTaskBase(), savedWorkOrder, null, task.getValue());
+                        copiedTask.setCompany(preventiveMaintenance.getCompany());
                         taskService.create(copiedTask);
                     });
                 }
