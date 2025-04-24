1b102ac6088041ee45b4ba03ef7a6f8ec0baf8c5
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-04-24 22:35:25 +0100

    fix: infinite loop while restoring table state

--- frontend/src/hooks/useGridStatePersist.ts
+++ frontend/src/hooks/useGridStatePersist.ts
@@ -1,15 +1,27 @@
-import React, { useState, useCallback, useEffect, useLayoutEffect } from 'react';
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef
+} from 'react';
 import { GridApiPro } from '@mui/x-data-grid-pro/models/gridApiPro';
 import { GridColDef } from '@mui/x-data-grid';
 
-const useGridStatePersist = (apiRef: React.MutableRefObject<GridApiPro>, columns: GridColDef[], prefix: string) => {
+const useGridStatePersist = (
+  apiRef: React.MutableRefObject<GridApiPro>,
+  columns: GridColDef[],
+  prefix: string
+) => {
   const stateItem = `${prefix}DataGridState`;
+  const hasRestoredRef = useRef(false);
+
   const saveSnapshot = useCallback(() => {
     if (apiRef?.current?.exportState && localStorage) {
       const currentState = apiRef.current.exportState();
       localStorage.setItem(stateItem, JSON.stringify(currentState));
     }
-  }, [apiRef]);
+  }, [apiRef, stateItem]);
 
   useLayoutEffect(() => {
     const handleBeforeUnload = () => {
@@ -25,11 +37,20 @@ const useGridStatePersist = (apiRef: React.MutableRefObject<GridApiPro>, columns
   }, [saveSnapshot]);
 
   useEffect(() => {
-    if (columns != null && apiRef?.current?.restoreState != null && localStorage?.getItem(stateItem)) {
+    if (
+      !hasRestoredRef.current &&
+      columns != null &&
+      apiRef?.current?.restoreState != null &&
+      localStorage?.getItem(stateItem)
+    ) {
       const state = JSON.parse(localStorage.getItem(stateItem));
-      apiRef?.current?.restoreState({ columns: state.columns });
+      apiRef?.current?.restoreState({
+        columns: state.columns,
+        sorting: state.sorting
+      });
+      hasRestoredRef.current = true;
     }
-  }, [apiRef, columns]);
+  }, [apiRef, columns, stateItem]);
 };
 
 export default useGridStatePersist;
