3f2e2d000584933a47bf6f71bb1fadefbc8cb3d0
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-05-21 01:19:14 +0100

    chore(mobile): versioning

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -7,7 +7,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Atlas CMMS',
   slug: 'atlas-cmms',
-  version: '1.0.27',
+  version: '1.0.28',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'atlascmms',
