062e19b08bcaff66a0eac67a305e02ecf9b4eefa
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2025-07-07 20:39:05 +0100

    fix: annual savings

--- frontend/src/content/pricing/components/SubscriptionPlanSelector.tsx
+++ frontend/src/content/pricing/components/SubscriptionPlanSelector.tsx
@@ -48,7 +48,7 @@ export default function SubscriptionPlanSelector({
             sx={{ transform: 'scale(1.3)' }}
             size={'medium'}
           />
-          <Typography>Annually (Save 20%)</Typography>
+          <Typography>Annually (Save 17%)</Typography>
         </Stack>
       </Box>
       <Grid container spacing={2} justifyContent="center">
